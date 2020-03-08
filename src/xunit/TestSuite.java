f7b50d201490c4fc46714d93f35b2736f862e096
임재성 <imjaeseong@imjaeseong-ui-MacBookPro.local>
2020-03-09 01:41:18 +0900

    first & last commit

---
+++ src/xunit/TestSuite.java
@@ -0,0 +1,39 @@
+package xunit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestSuite implements Test{
+    List<Test> tests = new ArrayList<>();
+
+    public TestSuite(Class<? extends Test> testClass) {
+        Arrays.stream(testClass.getMethods())
+//                .filter(m -> m.getName().startsWith("test"))
+                .filter(m -> m.getAnnotation(xunit.annotation.Test.class) != null)
+                .forEach( m ->
+                        {
+                            try {
+                                add(testClass.getConstructor(String.class).newInstance(m.getName()));
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+    }
+
+    public TestSuite() {
+
+    }
+
+    public void add(Test test) {
+        tests.add(test);
+    }
+
+    public void run(TestResult result) {
+        tests.forEach(t -> {
+            t.run(result);
+        });
+    }
+}
